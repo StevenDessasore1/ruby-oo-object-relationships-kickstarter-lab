a4d6520466fe31e708b828405881f14072e6674f
StevenDessasore <stevendessasoreba@gmail.com>
2021-04-07 03:42:12 -0400

    commit tree 12948560f53ffd66346f7e2543a6fd8e08bac383

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+class Backer
+    attr_reader :name;
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        project = ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        all_projectBaker = ProjectBacker.all.select do |project_backer|
+            project_backer.backer == self
+        end
+        all_projectBaker.map do |all_project|
+            all_project.project
+        end
+    end
+end
